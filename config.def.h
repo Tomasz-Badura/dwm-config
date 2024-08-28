ea6da3f89c1a2632e7af7f88635aec012192ed2f
Tomasz-Badura <tomaszbadurakontakt@gmail.com>
2024-08-28 19:06:47 +0200

    updated colors/font/icons

--- config.def.h
+++ config.def.h
@@ -10,13 +10,13 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Ubuntu Mono derivative Powerline:size=12", "Font Awesome 6 Free:size=12" };
+static const char dmenufont[]       = "Ubuntu Mono derivative Powerline:size=12";
+static const char col_gray1[]       = "#0B0E10";
+static const char col_gray2[]       = "#3A4444";
+static const char col_gray3[]       = "#4C4D4D";
+static const char col_gray4[]       = "#394142";
+static const char col_cyan[]        = "#000000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -24,7 +24,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -42,10 +42,10 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 static const int lockfullscreen = 0; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
-	{ "[]=",      tile },
-	{ "[M]",      monocle },
-	{ "|M|",      centeredmaster },
-	{ ">M>",      centeredfloatingmaster },
+	{ "[T]",      tile },
+	{ "[-]",      monocle },
+	{ "[M]",      centeredmaster },
+	{ "{M}",      centeredfloatingmaster },
 };
 
 /* key definitions */
