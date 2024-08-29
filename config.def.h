7ba44c5e3e39403d797e4644fe65574d4993ca8f
Tomasz-Badura <tomaszbadurakontakt@gmail.com>
2024-08-29 23:34:39 +0200

    fix keybind, test msedge custom opacity

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const double activeopacity   = 0.8f;     /* Window opacity when it's focused (0 <= opacity <= 1) */
-static const double inactiveopacity = 0.5f;     /* Window opacity when it's inactive (0 <= opacity <= 1) */
+static const double inactiveopacity = 0.8f;     /* Window opacity when it's inactive (0 <= opacity <= 1) */
 static const char *fonts[]          = { "Ubuntu Mono derivative Powerline:size=11", "Font Awesome 6 Free:size=10" };
 static const char dmenufont[]       = "Ubuntu Mono derivative Powerline:size=11";
 static const char col_gray1[]       = "#FFFFFF";
@@ -33,8 +33,8 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      			instance    title       tags mask	isfloating   focusopacity    unfocusopacity		monitor */
-	{ "microsoft-edge",     NULL,       NULL,       0,			0,           1.0,            1.0,   			-1 },
+	/* class      			instance    		title       tags mask	isfloating   focusopacity    unfocusopacity		monitor */
+{ 	"Microsoft-edge",     	"microsoft-edge",   NULL,       0,			0,           1.0,            1.0,   			-1 },
 };
 
 /* layout(s) */
@@ -70,8 +70,8 @@ static const char *termcmd[]  = { "wezterm", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        	function        		argument */
-	{ MODKEY,                       XK_w,      	spawn,          		{.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_w, 	   	spawn,          		{.v = termcmd } },
+	{ MODKEY,                       XK_q,      	spawn,          		{.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_q, 	   	spawn,          		{.v = termcmd } },
 	{ MODKEY,                       XK_b,      	togglebar,      		{0} },
 	{ MODKEY,                       XK_m,      	incnmaster,     		{.i = +1 } },
 	{ MODKEY,                       XK_n,      	incnmaster,     		{.i = -1 } },
