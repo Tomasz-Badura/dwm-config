78a3b0ad6ab2a6c9bc0132eb564d063c9a035098
Tomasz-Badura <tomaszbadurakontakt@gmail.com>
2024-09-01 19:11:16 +0200

    fix cmds

--- config.def.h
+++ config.def.h
@@ -72,8 +72,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] 	= { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  	= { "wezterm", NULL };
-static const char *rebootcmd[]	= { "prompt 'Reboot?' 'reboot'", NULL };
-static const char *shutdowncmd[]= { "prompt 'Shutdown?' 'shutdown 0'", NULL };
+static const char *rebootcmd[]	= { "prompt", "-p", "Reboot?", "-c", "reboot", NULL };
+static const char *shutdowncmd[]= { "prompt", "-p", "Shutdown?", "-c", "shutdown 0", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        	function        		argument */
